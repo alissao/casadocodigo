f789a002d40a5f1b2a4a155998ccb61e25c567d8
alissao <alissonhbv@gmail.com>
2020-03-25 18:32:00 -0300

    25/03

--- src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
+++ src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
@@ -1,23 +1,64 @@
 package br.com.casadocodigo.loja.controllers;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.List;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import br.com.casadocodigo.loja.daos.ProdutoDAO;
 import br.com.casadocodigo.loja.models.Produto;
+import br.com.casadocodigo.loja.models.TipoPreco;
 
 @Controller
+@RequestMapping("/produtos")
 public class ProdutosController {
 	
-	@RequestMapping("/produtos/form")
-	public String form() {
-		return "produtos/form";
+	@Autowired
+	private ProdutoDAO dao;
+	
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+		binder.addValidators(new ProdutoValidation());
 	}
 	
-	@RequestMapping("/produtos")
-	public String grava(Produto produto) {
-		System.out.println(produto);
+	@RequestMapping("/form")
+	public ModelAndView form() {
+		ModelAndView modelAndView = new ModelAndView("produtos/form");
+		modelAndView.addObject("tipos", TipoPreco.values());
+		return modelAndView;
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ModelAndView grava(@Valid Produto produto, BindingResult result, RedirectAttributes redirectAttributes) {
 		
-		return "produtos/ok";
+		if (result.hasErrors()) {
+			return form();
+		}
+		
+		dao.gravar(produto);
+//		ModelAndView modelAndView = new ModelAndView();
+		redirectAttributes.addFlashAttribute("sucesso", "Produto cadastrado com sucesso!");
+//		modelAndView.addObject();
+		return new ModelAndView("redirect:produtos");
 		
 	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ModelAndView listar() {
+		List<Produto> produtos = dao.listar();
+		ModelAndView modelAndView = new ModelAndView("produtos/lista");
+		modelAndView.addObject("produtos", produtos);
+		
+		return modelAndView;
+	}
+	
 }
--- src/main/java/br/com/casadocodigo/loja/models/Produto.java
+++ src/main/java/br/com/casadocodigo/loja/models/Produto.java
@@ -1,15 +1,25 @@
 package br.com.casadocodigo.loja.models;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
 
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Produto {
 	
-	@Autowired
-	private ProdutoDAO produtoDAO;
-
+	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
 	private String titulo;
 	private String descricao;
 	private int paginas;
+
+	@ElementCollection
+	private List<Preco> precos;
+	
 	public String getTitulo() {
 		return titulo;
 	}
@@ -28,12 +38,21 @@ public class Produto {
 	public void setPaginas(int paginas) {
 		this.paginas = paginas;
 	}
-	
 	@Override
 	public String toString() {
 		return "Produto [titulo=" + titulo + ", descricao=" + descricao + ", paginas=" + paginas + "]";
 	}
-	
-	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public List<Preco> getPrecos() {
+		return precos;
+	}
+	public void setPrecos(List<Preco> precos) {
+		this.precos = precos;
+	}
 	
 }
