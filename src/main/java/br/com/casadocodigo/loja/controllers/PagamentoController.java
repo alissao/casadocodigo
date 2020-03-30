f55a408125cb0501b3d4c9b3ec64de2fb505c961
alissao <alissonhbv@gmail.com>
2020-03-30 18:35:05 -0300

    30/03

--- src/main/java/br/com/casadocodigo/loja/controllers/PagamentoController.java
+++ src/main/java/br/com/casadocodigo/loja/controllers/PagamentoController.java
@@ -3,6 +3,9 @@ package br.com.casadocodigo.loja.controllers;
 import java.util.concurrent.Callable;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,6 +16,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.casadocodigo.loja.models.CarrinhoCompras;
 import br.com.casadocodigo.loja.models.DadosPagamento;
+import br.com.casadocodigo.loja.models.Usuario;
 
 @Controller
 @RequestMapping("/pagamento")
@@ -24,8 +28,12 @@ public class PagamentoController {
 	@Autowired
 	RestTemplate restTemplate;
 	
+	@Autowired
+	private MailSender sender;
+	
 	@RequestMapping(value="/finalizar", method=RequestMethod.POST)
-	public Callable<ModelAndView>  finalizar(RedirectAttributes model){
+	public Callable<ModelAndView>  finalizar(@AuthenticationPrincipal Usuario usuario, 
+		      RedirectAttributes model){
 		return () -> {
 			String uri = "http://book-payment.herokuapp.com/payment";
 			
@@ -33,6 +41,9 @@ public class PagamentoController {
 				String response = restTemplate.postForObject(uri, new DadosPagamento(carrinho.getTotal()), String.class);
 				model.addFlashAttribute("sucesso", response);
 				System.out.println(response);
+				
+				enviaEmailCompraProduto(usuario);enviaEmailCompraProduto(usuario);
+				
 				//método para tirar todos os livros do carrinho
 				this.carrinho.limpa();
 				return new ModelAndView("redirect:/produtos");
@@ -43,4 +54,16 @@ public class PagamentoController {
 			}
 		};
 	}
+	
+	private void enviaEmailCompraProduto(Usuario usuario) {
+		SimpleMailMessage email = new SimpleMailMessage();
+
+	    email.setSubject("Compra finalizada com sucesso");
+	    email.setTo(usuario.getEmail());
+	    email.setText("Compra aprovada com sucesso no valor de " + carrinho.getTotal());
+	    email.setFrom("compras@casadocodigo.com.br");
+
+	    sender.send(email);
+	}
+	
 }
